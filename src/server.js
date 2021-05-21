838010d3fdfdf2f3341fd693a357f6de3202e467
Justin <justinmhoch@gmail.com>
2021-05-21 17:32:45 -0400

    Added MongoClient and dotenv(to seperate mongodb URL from server.js) and tested the connection to mongodb using postman

--- src/server.js
+++ src/server.js
@@ -1,7 +1,34 @@
 import express from 'express';
+import MongoClient from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
+// tell express to parse JSON
+app.use(express.json());
+
+// Get tasks from MongoDb
+app.get('/tasks', async (req, res) => {
+  try {
+    const dbURL = process.env.DBURL;
+    const client = await MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = client.db('react-task-tracker');
+
+    const tasks = await db.collection('tasks').find( {} ).toArray();
+    res.status(200).json(tasks);
+    console.log("connection worked");
+
+    client.close();
+  } catch (error) {
+    res.status(500).json({message: 'Error connnecting to database', error });
+    console.log(error);
+  }
+  
+})
+
+// Testing server
 app.get('/hello', (req, res) => res.send('Hello'));
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 
 app.listen(8000, () => console.log('listening on port 8000'));
