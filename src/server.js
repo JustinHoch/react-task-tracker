30358ddbf17666eee041f4049d2fb6082be42b11
Justin <justinmhoch@gmail.com>
2021-05-22 13:32:06 -0400

    Made dbConnection function for DRY(Do Not Repeat yourself)

--- src/server.js
+++ src/server.js
@@ -8,15 +8,17 @@ const app = express();
 // tell express to parse JSON
 app.use(express.json());
 
-// Get tasks from MongoDb
-app.get('/tasks', async (req, res) => {
+// Database Connection
+const dbConnection = async (operations, res) => {
   try {
     const dbURL = process.env.DBURL;
     const client = await MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = client.db('react-task-tracker');
 
-    const tasks = await db.collection('tasks').find( {} ).toArray();
-    res.status(200).json(tasks);
+    // For queries
+    await operations(db);
+
+    // For testing REMOVE LATER
     console.log("connection worked");
 
     client.close();
@@ -24,9 +26,26 @@ app.get('/tasks', async (req, res) => {
     res.status(500).json({message: 'Error connnecting to database', error });
     console.log(error);
   }
+};
+
+// Get tasks from MongoDb
+app.get('/tasks', async (req, res) => {
+  dbConnection(async (db) => {
+
+    // Query: get all tasks from 'tasks' collection
+    const tasks = await db.collection('tasks').find({}).toArray();
+
+    // Send tasks in json format
+    res.status(200).json(tasks);
+
+  }, res);
 });
 
 // Add Task
 
+// Delete Task
 
+// Set Task Reminder
+
+// Server listening on port 8000
 app.listen(8000, () => console.log('listening on port 8000'));
